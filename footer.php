daa0339a3533ff288988d69762c0908fc856963d
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-02 16:05:16 +0700

    Update footer code

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id="footer">
+	<div id="footer-inner">&copy; 2018 AnhChangThoDe22145 All Rights Reserved.</div>
+</div><!-- End of footer -->
+<?php if (is_user_loggin()) : ?>
+	</div><!-- End of content -->
+</div><!-- End of content-wrap -->
+</div><!-- End of container -->
+<script src="<?php echo SITE_URL;?>js/script.js"></script>
+<?php endif; ?>
+</body>
+</html>
