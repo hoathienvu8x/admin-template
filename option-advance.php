1f92f62c15ead6c175824b4ab3d88c8dd8fb5f83
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-02 14:34:15 +0700

    Update option form elements fields

--- option-advance.php
+++ option-advance.php
@@ -1,3 +1,31 @@
+<?php $args = array(); ?>
 <?php require_once 'header.php'; ?>
-
+<form action="<?php echo Url::option($args); ?>" method="post"  enctype="multipart/form-data">
+	<div class="message success"><p>Option saved</p></div>
+	<div class="form-inner form-elements">
+		<table class="horizontal-display">
+			<tr>
+				<td class="row-label">Option name</td>
+				<td>
+					<p><input type="text" name="option_name" value="" placeholder="Option name" /></p>
+				</td>
+			</tr>
+			<tr>
+				<td class="row-label">Description</td>
+				<td>
+					<p><input type="text" name="option_desc" value="" placeholder="Description" /></p>
+				</td>
+			</tr>
+			<tr>
+				<td class="row-label">Value</td>
+				<td>
+					<p><textarea name="option_value" placeholder="Value"></textarea></p>
+				</td>
+			</tr>
+		</table>
+	</div>
+	<div class="form-inner form-button">
+		<input type="submit" name="save" value="Save" />
+	</div>
+</form>
 <?php require_once 'footer.php'; ?>
