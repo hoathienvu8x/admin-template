50ab530c35f94bb8265321a6c5a489885db78c54
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-02 16:18:32 +0700

    add change copyright year

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-	<div id="footer-inner">&copy; 2018 AnhChangThoDe22145 All Rights Reserved.</div>
+	<div id="footer-inner">&copy; 2018<?php echo date('Y') > 2018 ? '-'.date('y') : ''; ?> AnhChangThoDe22145 All Rights Reserved.</div>
 </div><!-- End of footer -->
 <?php if (is_user_loggin()) : ?>
 	</div><!-- End of content -->
