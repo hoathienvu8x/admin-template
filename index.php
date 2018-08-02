0fc8766539797b6ecbb55335d4e6ef147bd4fc86
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-02 11:32:46 +0700

    Update index router code

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+if (!is_user_loggin()) {
+	require_once 'login.php';
+	exit;
+}
+$action = isset($_GET['action']) ? trim($_GET['action']) : '';
+if (file_exists($action . '.php')) {
+	require_once $action . '.php';
+	exit;
+}
+?>
 <?php require_once 'header.php'; ?>
 
 <?php require_once 'footer.php'; ?>
