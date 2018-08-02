8185a27d1f1b45d4a7df6c0606c0b0154bec0f59
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-02 14:10:32 +0700

    Update login screen UI

--- login.php
+++ login.php
@@ -1,3 +1,19 @@
 <?php require_once 'header.php'; ?>
-
+<form action="<?php echo Url::home(); ?>" method="post">
+	<div id="login-form">
+		<div id="login-logo"><img src="images/logo.png" /></div>
+		<p><input type="text" name="username" value="" placeholder="Username" /></p>
+		<p><input type="password" name="password" value="" placeholder="Password" /></p>
+		<p>
+			<img src="capcha.php" />
+			<input type="text" name="code" value="" placeholder="Code" />
+		</p>
+		<p><label for="remember_me"><input type="checkbox" name="remember" value="y" id="remember_me" /> Remember me ?</label></p>
+		<div class="login-button">
+			<a href="<?php echo Url::forgot(); ?>">Loss password ?</a>
+			<input type="submit" name="login" value="Login" />
+		</div>
+	</div>
+	<div class="message error"><p>Please login !</p></div>
+</form>
 <?php require_once 'footer.php'; ?>
