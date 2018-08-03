a55123c8da517808e460bb2927314a89fd55ab1c
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-03 15:49:15 +0700

    Add check ajax doing from jquery or ajax with header of ajax is xmlhttprequest

--- init.php
+++ init.php
@@ -3,4 +3,7 @@ if (!defined('INAPP')) exit;
 define('SITE_URL', './');
 define('BYTES_PER_CHUNK', 1024);
 require_once 'functions.php';
+if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+	define('AJAX_DOING', true);
+}
 ?>
