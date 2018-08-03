0ea8a922302771e56e444c75ca43bd66dbc2c89d
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-03 13:34:26 +0700

    update login style and responsive form

--- login.php
+++ login.php
@@ -3,10 +3,10 @@
 <form action="<?php echo Url::home(); ?>" method="post">
 	<div id="login-form">
 		<div id="login-logo"><img src="images/logo.svg" /></div>
-		<p><input type="text" name="username" value="" placeholder="Username" /></p>
-		<p><input type="password" name="password" value="" placeholder="Password" /></p>
+		<p><input type="text" name="username" value="" placeholder="Username" autocapitalize="off" autocomplete="off" /></p>
+		<p><input type="password" name="password" value="" placeholder="Password" autocapitalize="off" autocomplete="off" /></p>
 		<p>
-			<input type="text" name="code" value="" placeholder="Code" class="code" />
+			<input type="text" name="code" value="" placeholder="Code" class="code" autocapitalize="off" autocomplete="off" />
 			<img src="capcha.php?mode=t" />
 		</p>
 		<p><label for="remember_me"><input type="checkbox" name="remember" value="y" id="remember_me" /> Remember me ?</label></p>
