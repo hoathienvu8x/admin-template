8f6917e872d9102e317352a497179fbd1dc6141c
Hoa Thien Vu <hoathienvu8x@gmail.com>
2018-08-03 15:27:13 +0700

    update make command line

--- make.sh
+++ make.sh
@@ -2,8 +2,9 @@
 
 cp -rf * /home/hoathienvu8x/Desktop/local/admin/template
 
-mv /home/hoathienvu8x/Desktop/local/admin/template/upload.php /home/hoatienvu8x/local/admin
-mv /home/hoathienvu8x/Desktop/local/admin/template/ajax.php /home/hoatienvu8x/local/admin
+cd /home/hoathienvu8x/Desktop/local/admin/
+
+mv -t . template/upload.php template/ajax.php
 
 
 
